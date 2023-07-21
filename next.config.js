4795493ae72ff20f75e051311f19ff2254d2f1ed
Naresh kumar <nareshxo@Nareshs-Air.wind3.hub>
2023-07-21 19:28:56 +0200

    bug fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "tailwindui.com",
-      "res.cloudinary.com"
-    ]
-  }
-}
+    domains: ["tailwindui.com", "res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  experimental: {
+    appDir: false,
+  },
+};
